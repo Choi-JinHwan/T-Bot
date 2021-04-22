51e618cec0d790410bb7e091a8aba8577eda703d
Choi-JinHwan <jinhwan0515@gamil.com>
2021-04-23 00:08:08 +0900

    feat: 유저채팅 trace level event logging 추가

--- src/main/kotlin/dev/light/bot/presentation/TwitchUserChatListener.kt
+++ src/main/kotlin/dev/light/bot/presentation/TwitchUserChatListener.kt
@@ -2,15 +2,22 @@ package dev.light.bot.presentation
 
 import com.github.twitch4j.chat.TwitchChat
 import dev.light.bot.domain.TwitchUserChatEvent
+import org.slf4j.LoggerFactory
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 class TwitchUserChatListener(val twitchChat: TwitchChat) {
 
+    companion object {
+        private val logger = LoggerFactory.getLogger(this.javaClass)
+    }
+
     @EventListener
-    fun printListen(userUserChatEvent: TwitchUserChatEvent) {
-        println("$userUserChatEvent")
+    fun loggingListener(userChatEvent: TwitchUserChatEvent) {
+        if (logger.isTraceEnabled) {
+            logger.trace("{}", userChatEvent)
+        }
     }
 
 }
